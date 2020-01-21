c2005ad545e47636ecc1ba3cb6edbe4e7c6f56a7
your_name <your_email>
2020-01-21 15:17:30 +0800

    test

--- src/main/java/com/chen/github/GithubTestApplication.java
+++ src/main/java/com/chen/github/GithubTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubTestApplication {
 
     public static void main(String[] args) {
+        //test
         SpringApplication.run(GithubTestApplication.class, args);
     }
 }
